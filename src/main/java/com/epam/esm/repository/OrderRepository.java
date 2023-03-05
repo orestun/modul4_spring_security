a5c949794e057d68c17f2d039b8763ba92fe6030
orestun <orest.uzhytchak0001@gmail.com>
2023-03-05 22:54:17 +0200

    Added query for finding orders

--- src/main/java/com/epam/esm/repository/OrderRepository.java
+++ src/main/java/com/epam/esm/repository/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.epam.esm.repository;
 
 import com.epam.esm.entity.Order;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByUserID(Long userID, PageRequest of);
+
+    @Query("select o from Order o where o.id = ?1 and o.userID=?2")
+    Order findOrderByOrderIdForUserByUserId(Long orderId, Long userId);
 }
