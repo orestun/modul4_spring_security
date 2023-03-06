e8f5683479996fd9e615b889daf9fa1da6018878
orestun <orest.uzhytchak0001@gmail.com>
2023-03-06 12:56:18 +0200

    Added a new link for getting all orders for user by id

--- src/main/java/com/epam/esm/hateoas/UserHateoas.java
+++ src/main/java/com/epam/esm/hateoas/UserHateoas.java
@@ -13,16 +13,25 @@ public class UserHateoas {
                     withRel("Get all users").
                     withType("GET");
 
+    private static Link getOrdersForUserByIdLink(Long userId){
+        return Link.of(String.format("http://localhost:8080/user/%d/orders",userId)).
+                withRel(String.format("Get all orders for user with (id = %d)", userId)).
+                withType("GET");
+    }
+
+
     /**
      * Hateoas method that add links to Tag objects got as
-     * result of method {@link UserController#addNewUser(UserDTO)}
+     * result of method {@link com.epam.esm.controller.UserController#addNewUser(UserDto)}
      *
      * @param user user object that was added in DB
      *
      * @return user object with links
      * */
     static public UserDto linksForAddingNewUser(UserDto user){
-        user.add(getAllUsersLink);
+        user.add(
+                getAllUsersLink,
+                getOrdersForUserByIdLink(user.getId()));
         return user;
     }
 }
