33870ab8bce456281a135441c109d5d584baa96f
orestun <orest.uzhytchak0001@gmail.com>
2023-03-20 12:17:55 +0200

    Added user details implementation class

---
+++ src/main/java/com/epam/esm/entity/UserDetailsImpl.java
@@ -0,0 +1,66 @@
+package com.epam.esm.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class UserDetailsImpl implements UserDetails {
+
+    private String email;
+    private String password;
+    private List<GrantedAuthority> roles;
+
+    public UserDetailsImpl(User user) {
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.roles = user.
+                getRoles().
+                stream().
+                map(Role::getRole).
+                map(SimpleGrantedAuthority::new).
+                collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
