a4ca04ce8345997a0ab15f8fe0ba0246b6ca6ea8
orestun <orest.uzhytchak0001@gmail.com>
2023-03-20 12:31:44 +0200

    Added user details service implementation class

--- src/main/java/com/epam/esm/repository/UserRepository.java
+++ src/main/java/com/epam/esm/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.epam.esm.repository;
 import com.epam.esm.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
