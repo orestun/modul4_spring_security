7eb05f5938ddc1d34ee8c40a09b2e1324682b22f
orestun <orest.uzhytchak0001@gmail.com>
2023-04-28 13:52:39 +0300

    removed some unused imports and methods

--- src/main/java/com/epam/esm/repository/GiftCertificateRepository.java
+++ src/main/java/com/epam/esm/repository/GiftCertificateRepository.java
@@ -4,13 +4,9 @@ import com.epam.esm.entity.GiftCertificate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -37,17 +33,6 @@ public interface GiftCertificateRepository extends JpaRepository<GiftCertificate
             @Param(value = "tags") Set<String> tags,
             @Param(value = "pageFrom") Integer pageFrom,
             @Param(value = "pageTo") Integer pageTo);
-
-    @Transactional
-    @Modifying
-    @Query("update GiftCertificate g set g.name = ?2, g.description = ?3, g.duration = ?4, g.price = ?5, g.createDate = ?6, g.updateDate = ?7 where g.id = ?1")
-    void updateGiftCertificateById(Long id,
-                                   String name,
-                                   String description,
-                                   int duration,
-                                   BigDecimal price,
-                                   LocalDate createDate,
-                                   LocalDate updateDate);
 
     Optional<GiftCertificate> findById(Long aLong);
 
