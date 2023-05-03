45d45d36f3a113ce1ce5bbffeed4ec22c9e1f331
orestun <orest.uzhytchak0001@gmail.com>
2023-05-03 13:54:00 +0300

    changed packaging jar -> war

--- src/main/java/com/epam/esm/SpringSecurityApplication.java
+++ src/main/java/com/epam/esm/SpringSecurityApplication.java
@@ -3,6 +3,8 @@ package com.epam.esm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,8 +14,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableJpaAuditing
 @EnableWebSecurity
 @SpringBootApplication
-public class SpringSecurityApplication {
-
+public class SpringSecurityApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringSecurityApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
